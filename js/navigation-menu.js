1a274613baf26daebed2c5d5d602dfc7ead81202
Pie with jam <105319693+pie-with-jam@users.noreply.github.com>
2023-02-26 19:01:42 +0300

    Add files via upload

---
+++ js/navigation-menu.js
@@ -0,0 +1,25 @@
+let sections = $('section'), 
+nav = $('nav'), 
+nav_height = nav.outerHeight();
+$(window).on('scroll', function () {
+    $('nav a').removeClass('active');
+    let cur_pos = $(this).scrollTop(); 
+    sections.each(function() {
+        let top = $(this).offset().top - nav_height - 180,
+        bottom = top + $(this).outerHeight();       
+        if (cur_pos >= top && cur_pos <= bottom) {
+            nav.find('a').removeClass('active');
+            sections.removeClass('active');    
+            $(this).addClass('active');
+            nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+        }
+    });
+});
+nav.find('a').on('click', function () {
+    let $el = $(this), 
+    id = $el.attr('href'); 
+    $('html, body').animate({
+        scrollTop: $(id).offset().top - nav_height
+    }, 600);
+    return false;
+});
