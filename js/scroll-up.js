c7a5205639ad0773c8bba5c342081b16ef9fd130
Pie with jam <105319693+pie-with-jam@users.noreply.github.com>
2023-03-02 20:05:38 +0300

    Add files via upload

--- js/scroll-up.js
+++ js/scroll-up.js
@@ -1,18 +1,1 @@
-$('body').append('<div class="upbtn"></div>');            
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-        $('.upbtn').css({
-            bottom: '15px'
-        });
-        } else {
-        $('.upbtn').css({
-            bottom: '-80px'
-        });
-    }
-});
-$('.upbtn').on('click',function() {
-    $('html, body').animate({
-        scrollTop: 0
-    }, 500);
-    return false;
-});
+$('body').append('<div class="upbtn"></div>');$(window).scroll(function(){if($(this).scrollTop()>100){$('.upbtn').css({bottom:'15px'});}else{$('.upbtn').css({bottom:'-80px'});}});$('.upbtn').on('click',function(){$('html, body').animate({scrollTop:0},500);return false;});
