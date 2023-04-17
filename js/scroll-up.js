1d9bb362c3932842a67335a5ee21f7b28c1a28d2
Clowdyyy <106470599+Clowdyyy@users.noreply.github.com>
2023-04-17 13:30:37 +0500

    Delete js directory

--- js/scroll-up.js
+++
@@ -1,1 +0,0 @@
-$('body').append('<div class="upbtn"></div>');$(window).scroll(function(){if($(this).scrollTop()>100){$('.upbtn').css({bottom:'15px'});}else{$('.upbtn').css({bottom:'-80px'});}});$('.upbtn').on('click',function(){$('html, body').animate({scrollTop:0},500);return false;});
